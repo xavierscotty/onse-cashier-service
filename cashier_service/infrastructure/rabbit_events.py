13b13a9b2ca566933f7be9348d3f7a239f839885
Tom Oram <tom.oram@armakuni.com>
2019-01-14 18:53:19 +0000

    [#163196759] Attempt to reconnect to Rabbit on error

--- cashier_service/infrastructure/rabbit_events.py
+++ cashier_service/infrastructure/rabbit_events.py
@@ -1,37 +1,36 @@
 from pika import ConnectionParameters, BlockingConnection
+from pika.exceptions import AMQPConnectionError
 
 
-class RabbitConnection:
+class RabbitBroker:
     def __init__(self, config):
-        self.queue = config.RABBITMQ_QUEUE
+        self._config = config
+        self._exchange = config.RABBITMQ_EXCHANGE_NAME
+
+        self._init_connection(config)
+
+    def produce(self, event):
+        try:
+            self._get_channel().basic_publish(exchange=self._exchange,
+                                              routing_key='',
+                                              body=event)
+        except AMQPConnectionError:
+            self._init_connection(self._config)
+
+            self._get_channel().basic_publish(exchange=self._exchange,
+                                              routing_key='',
+                                              body=event)
+
+    def _init_connection(self, config):
         params = ConnectionParameters(
             host=config.RABBITMQ_HOST,
             heartbeat_interval=int(config.RABBITMQ_HEARTBEAT_INTERVAL),
             blocked_connection_timeout=int(config.RABBITMQ_HEARTBEAT_INTERVAL))
+
         self._connection = BlockingConnection(params)
         self._channel = self._connection.channel()
-        self._channel.queue_declare(queue=self.queue)
 
-    def get_channel(self):
+        self._channel.exchange_declare(self._exchange, exchange_type='fanout')
+
+    def _get_channel(self):
         return self._connection.channel()
-
-
-class RabbitBroker(RabbitConnection):
-    def __init__(self, config):
-        super().__init__(config)
-
-        self.exchange = config.RABBITMQ_EXCHANGE_NAME
-        self._channel.exchange_declare(
-            self.exchange, exchange_type=config.RABBITMQ_EXCHANGE_TYPE)
-        self._channel.queue_bind(queue=self.queue, exchange=self.exchange)
-        self._channel.close()
-
-    def produce(self, event):
-        """
-        Method that will publish messages and handle connections
-        """
-        channel = self.get_channel()
-        channel.basic_publish(exchange=self.exchange,
-                              routing_key=self.queue,
-                              body=event)
-        channel.close()
--- cashier_service/settings.py
+++ cashier_service/settings.py
@@ -6,24 +6,23 @@ from os import environ
 class Config(object):
     """Base configuration."""
     # SWAGGER
-    SWAGGER_URL = environ.get('SWAGGER_URL') or '/docs'
-    SWAGGER_FILE_PATH = environ.get('SWAGGER_FILE_PATH') or '/swagger.yml'
+    SWAGGER_URL = environ.get('SWAGGER_URL', '/docs')
+    SWAGGER_FILE_PATH = environ.get('SWAGGER_FILE_PATH', '/swagger.yml')
     # APPLICATION
-    APP_NAME = environ.get('APP_NAME') or 'Cashier Service API'
-    PORT = environ.get('PORT') or 5001
+    APP_NAME = environ.get('APP_NAME', 'Cashier Service API')
+    PORT = environ.get('PORT', 5001)
 
 
 class DevConfig(Config):
     """Development configuration."""
     # RabbitMQ
-    RABBITMQ_HOST = environ.get('RABBITMQ_HOST') or ''
-    RABBITMQ_QUEUE = environ.get('RABBITMQ_QUEUE') or ''
-    RABBITMQ_EXCHANGE_NAME = environ.get('RABBITMQ_EXCHANGE_NAME') or ''
-    RABBITMQ_EXCHANGE_TYPE = environ.get('RABBITMQ_TYPE') or 'topic'
-    RABBITMQ_HEARTBEAT_INTERVAL = environ.get(
-        'RABBITMQ_HEARTBEAT_INTERVAL') or 600
-    RABBITMQ_CONNECTION_TIMEOUT = environ.get(
-        'RABBITMQ_CONNECTION_TIMEOUT') or 300
+    RABBITMQ_HOST = environ.get('RABBITMQ_HOST', '')
+    RABBITMQ_EXCHANGE_NAME = environ.get('RABBITMQ_EXCHANGE_NAME', '')
+    RABBITMQ_EXCHANGE_TYPE = environ.get('RABBITMQ_TYPE', 'fanout')
+    RABBITMQ_HEARTBEAT_INTERVAL = environ.get('RABBITMQ_HEARTBEAT_INTERVAL',
+                                              600)
+    RABBITMQ_CONNECTION_TIMEOUT = environ.get('RABBITMQ_CONNECTION_TIMEOUT',
+                                              300)
 
     @classmethod
     def init_app(cls, app):
